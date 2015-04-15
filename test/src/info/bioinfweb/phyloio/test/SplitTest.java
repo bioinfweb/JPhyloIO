18eccc78ea1a7f6bb622aef3e372e43eee17c710
BenStoever <stoever@bioinfweb.info>
2015-04-15 07:35:51 +0000

    SplitTest and general test package added.

---
+++ test/src/info/bioinfweb/phyloio/test/SplitTest.java
@@ -0,0 +1,30 @@
+/*
+ * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
+ * Copyright (C) 2014-2015  Ben Stöver
+ * <http://bioinfweb.info/LibrAlign>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.phyloio.test;
+
+
+
+public class SplitTest {
+	public static void main(String[] args) {
+		String[] parts = "  \t 18  20.2\t ".trim().split("\\s+");
+		for (int i = 0; i < parts.length; i++) {
+			System.out.println("'" + parts[i] + "'");
+		}
+	}
+}
