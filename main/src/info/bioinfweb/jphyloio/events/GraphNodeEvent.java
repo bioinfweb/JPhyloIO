3c939df3c8516dd8f176f2616d1b7b97d5723cba
BenStoever <stoever@bioinfweb.info>
2016-01-04 16:15:00 +0000

    JavaDoc added to GraphNodeEvent.

--- main/src/info/bioinfweb/jphyloio/events/GraphNodeEvent.java
+++ main/src/info/bioinfweb/jphyloio/events/GraphNodeEvent.java
@@ -20,23 +20,61 @@ package info.bioinfweb.jphyloio.events;
 
 
 
+/**
+ * Event that indicates the definition of a tree node in a document. Different nodes maybe linked by
+ * subsequent {@link GraphEdgeEvent}s.
+ * 
+ * @author Ben St&ouml;ver
+ * @since 0.0.0
+ */
 public class GraphNodeEvent extends ConcreteJPhyloIOEvent {
 	private String id;
 	private String label;
 	
 	
-	public GraphNodeEvent(EventType eventType, String id, String label) {
+	/**
+	 * Creates a new instance of this event.
+	 * 
+	 * @param id an ID of this node which is unique for the current document
+	 * @param label an optional label the represented node is carrying (maybe {@code null}).
+	 */
+	public GraphNodeEvent(String id, String label) {
 		super(EventType.GRAPH_NODE);
-		this.id = id;
-		this.label = label;
+		
+		if (id == null) {
+			throw new NullPointerException("The ID must not be null.");
+		}
+		else {
+			this.id = id;
+			this.label = label;
+		}
 	}
 
 
+	/**
+	 * An ID identifying this node, which is unique for this document. If a format defines IDs (e.g. NeXML 
+	 * or XTG), these are used here. For formats not defining any IDs, the parser will generate its own IDs.
+	 * <p>
+	 * The ID returned here will also be referenced in up-coming {@link GraphEdgeEvent}s connecting two nodes.
+	 * (Applications based on JPhyloIO need this ID only to process up-coming {@link GraphEdgeEvent}s and 
+	 * do not necessarily have to store that IDs in their data model. Different IDs may be used for writing
+	 * the data in a possible subsequent step.)  
+	 * 
+	 * @return the ID of this node (unique for this document)
+	 */
 	public String getID() {
 		return id;
 	}
 
 
+	/**
+	 * Returns the label attached to this node.
+	 * <p>
+	 * Note that additional annotations of this node may be specified by up-coming metaevents, even if the node
+	 * carries no label.
+	 * 
+	 * @return the label string of {@code null} if this node carries no label
+	 */
 	public String getLabel() {
 		return label;
 	}
