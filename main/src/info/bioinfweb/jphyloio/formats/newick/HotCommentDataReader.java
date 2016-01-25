23b2b15d5d271805657775fd7f0330bee86edab6
BenStoever <stoever@bioinfweb.info>
2016-01-25 13:42:26 +0000

    Copyright information adjusted.

--- main/src/info/bioinfweb/jphyloio/formats/newick/HotCommentDataReader.java
+++ main/src/info/bioinfweb/jphyloio/formats/newick/HotCommentDataReader.java
@@ -1,14 +1,14 @@
 /*
- * TreeGraph 2 - A feature rich editor for phylogenetic trees
- * Copyright (C) 2015-2016  Ben Stöver, Sarah Wiechers, Kai Müller
- * <http://treegraph.bioinfweb.info/>
+ * JPhyloIO - Event based parsing and stream writing of multiple sequence alignment and tree formats. 
+ * Copyright (C) 2015-2016  Ben Stöver, Sarah Wiechers
+ * <http://bioinfweb.info/JPhyloIO>
  * 
- * This program is free software: you can redistribute it and/or modify
+ * This file is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
+ * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
