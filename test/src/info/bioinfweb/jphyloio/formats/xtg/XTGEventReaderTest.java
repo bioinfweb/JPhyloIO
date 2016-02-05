17208cdff84750b74029db6ae6c1a2b4b542df27
SarahWiechers <wiechers@bioinfweb.info>
2016-02-05 14:06:16 +0000

    Added XTGEventReaderTest.

---
+++ test/src/info/bioinfweb/jphyloio/formats/xtg/XTGEventReaderTest.java
@@ -0,0 +1,49 @@
+/*
+ * JPhyloIO - Event based parsing and stream writing of multiple sequence alignment and tree formats. 
+ * Copyright (C) 2015-2016  Ben Stöver, Sarah Wiechers
+ * <http://bioinfweb.info/JPhyloIO>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.jphyloio.formats.xtg;
+
+import static org.junit.Assert.fail;
+import info.bioinfweb.jphyloio.events.JPhyloIOEvent;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class XTGEventReaderTest {
+	@Test
+	public void testOutputNeXML() {
+		try {
+			XTGEventReader reader = new XTGEventReader(new File("data/XTG/ExampleTGDocument.xml"));
+			try {
+				while (reader.hasNextEvent()) {
+					JPhyloIOEvent event = reader.next();
+					System.out.println(event.getType().getContentType() + " " + event.getType().getTopologyType());
+				}
+			}
+			finally {
+				reader.close();
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getLocalizedMessage());
+		}
+	}
+
+}
