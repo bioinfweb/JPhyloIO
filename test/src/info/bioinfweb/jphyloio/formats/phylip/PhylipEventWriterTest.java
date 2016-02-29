7097ee4a62b29d22929afb7c286606292e1078d3
BenStoever <stoever@bioinfweb.info>
2016-02-29 16:04:50 +0000

    PhylipEventWriterTest added.

---
+++ test/src/info/bioinfweb/jphyloio/formats/phylip/PhylipEventWriterTest.java
@@ -0,0 +1,60 @@
+/*
+ * JPhyloIO - Event based parsing and stream writing of multiple sequence alignment and tree formats. 
+ * Copyright (C) 2015-2016  Ben Stöver, Sarah Wiechers
+ * <http://bioinfweb.info/JPhyloIO>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.jphyloio.formats.phylip;
+
+
+import static info.bioinfweb.jphyloio.test.JPhyloIOTestTools.createTestDocument;
+import static org.junit.Assert.assertEquals;
+
+
+import info.bioinfweb.jphyloio.EventWriterParameterMap;
+import info.bioinfweb.jphyloio.dataadapters.DocumentDataAdapter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import org.junit.Test;
+
+
+
+public class PhylipEventWriterTest {
+	@Test
+	public void test_writeDocument() throws Exception {
+		File file = new File("data/testOutput/Test.phy");
+		
+		// Write file:
+		DocumentDataAdapter document = createTestDocument("ACTGC", "A-TCC");
+		PhylipEventWriter writer = new PhylipEventWriter();
+		writer.writeDocument(document, file, new EventWriterParameterMap());
+		
+		// Validate file:
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		try {
+			assertEquals(" 2 5", reader.readLine());
+			assertEquals("Sequence 0ACTGC", reader.readLine());
+			assertEquals("Sequence 1A-TCC", reader.readLine());
+			assertEquals(-1, reader.read());
+		}
+		finally {
+			reader.close();
+			file.delete();
+		}
+	}
+}
