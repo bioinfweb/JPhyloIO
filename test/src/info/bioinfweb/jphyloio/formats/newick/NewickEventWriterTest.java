d8a4647ed638ff62dc35906c58d8fd9a4e0ba6da
BenStoever <stoever@bioinfweb.info>
2016-03-02 16:56:46 +0000

    Test case for OTU linked nodes added to NewickEventWriterTest.

--- test/src/info/bioinfweb/jphyloio/formats/newick/NewickEventWriterTest.java
+++ test/src/info/bioinfweb/jphyloio/formats/newick/NewickEventWriterTest.java
@@ -20,10 +20,11 @@ package info.bioinfweb.jphyloio.formats.newick;
 
 
 import static org.junit.Assert.*;
-
-
 import info.bioinfweb.jphyloio.ReadWriteParameterMap;
 import info.bioinfweb.jphyloio.dataadapters.implementations.ListBasedDocumentDataAdapter;
+import info.bioinfweb.jphyloio.events.LabeledIDEvent;
+import info.bioinfweb.jphyloio.events.type.EventContentType;
+import info.bioinfweb.jphyloio.test.dataadapters.TestOTUListDataAdapter;
 import info.bioinfweb.jphyloio.test.dataadapters.TestTreeDataAdapter;
 
 import java.io.BufferedReader;
@@ -51,6 +52,41 @@ public class NewickEventWriterTest {
 		try {
 			assertEquals("[&R] ((Node_t0nA:1.1[&annotation=100], Node_t0nB:0.9)'Node ''_1'[&a1=100, a2='ab ''c']:1.0, Node_t0nC:2.0)Node_t0nRoot:1.5;", reader.readLine());
 			assertEquals("[&R] ((Node_t1nA:1.1[&annotation=100], Node_t1nB:0.9)'Node ''_1'[&a1=100, a2='ab ''c']:1.0, Node_t1nC:2.0)Node_t1nRoot:1.5;", reader.readLine());
+			assertEquals(-1, reader.read());
+		}
+		finally {
+			reader.close();
+			file.delete();
+		}
+	}
+	
+	
+	@Test
+	public void test_writeDocument_linkedOTUs() throws Exception {
+		File file = new File("data/testOutput/LinkedOTUs.nwk");
+		
+		// Write file:
+		ListBasedDocumentDataAdapter document = new ListBasedDocumentDataAdapter();
+		
+		TestOTUListDataAdapter otuList = new TestOTUListDataAdapter(0, 
+				new LabeledIDEvent(EventContentType.OTU, "otu0", "label1"),
+				new LabeledIDEvent(EventContentType.OTU, "otu1", null),
+				new LabeledIDEvent(EventContentType.OTU, "otu2", "otu0"));
+		document.getOTUListsMap().put(otuList.getListStartEvent().getID(), otuList);
+		
+		document.getTreesNetworks().add(new TestTreeDataAdapter("tree0", "first tree", "t0"));
+		TestTreeDataAdapter tree = new TestTreeDataAdapter("tree1", "second tree", "t1", new String[]{"otu0", "otu1", "otu2"});
+		tree.setLinkedOTUsID("otus0");
+		document.getTreesNetworks().add(tree);
+		
+		NewickEventWriter writer = new NewickEventWriter();
+		writer.writeDocument(document, file, new ReadWriteParameterMap());
+		
+		// Validate file:
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		try {
+			assertEquals("[&R] ((Node_t0nA:1.1[&annotation=100], Node_t0nB:0.9)'Node ''_1'[&a1=100, a2='ab ''c']:1.0, Node_t0nC:2.0)Node_t0nRoot:1.5;", reader.readLine());
+			assertEquals("[&R] ((label1:1.1[&annotation=100], Node_t1nB:0.9)'Node ''_1'[&a1=100, a2='ab ''c']:1.0, otu0:2.0)Node_t1nRoot:1.5;", reader.readLine());
 			assertEquals(-1, reader.read());
 		}
 		finally {
