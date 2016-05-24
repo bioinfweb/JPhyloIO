f33af137859b6e08899ddbf882c17edd21369ffc
BenStoever <stoever@bioinfweb.info>
2016-05-24 20:14:55 +0000

    URIOrStringIdentifier.toString() added.

--- main/src/info/bioinfweb/jphyloio/events/meta/URIOrStringIdentifier.java
+++ main/src/info/bioinfweb/jphyloio/events/meta/URIOrStringIdentifier.java
@@ -46,6 +46,12 @@ public class URIOrStringIdentifier {
 
 
 	@Override
+	public String toString() {
+		return "(" + getURI() + ", " + getStringRepresentation() + ")";
+	}
+
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
