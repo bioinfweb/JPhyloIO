9d73e3b29cfc14ce473f65dc04ab598f11202d16
BenStoever <stoever@bioinfweb.info>
2016-06-03 12:21:03 +0000

    NeXMLConstants.DEFAULT_TOKEN_DEFINITION_SET_ID changed. Minor changes.

--- main/src/info/bioinfweb/jphyloio/formats/nexml/nexmlreceivers/NeXMLMolecularDataTokenDefinitionReceiver.java
+++ main/src/info/bioinfweb/jphyloio/formats/nexml/nexmlreceivers/NeXMLMolecularDataTokenDefinitionReceiver.java
@@ -1,18 +1,18 @@
 /*
- * JPhyloIO - Event based parsing and stream writing of multiple sequence alignment and tree formats. 
+ * JPhyloIO - Event based parsing and stream writing of multiple sequence alignment and tree formats.
  * Copyright (C) 2015-2016  Ben Stöver, Sarah Wiechers
  * <http://bioinfweb.info/JPhyloIO>
- * 
+ *
  * This file is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -51,67 +51,67 @@ import javax.xml.stream.XMLStreamWriter;
 public class NeXMLMolecularDataTokenDefinitionReceiver extends AbstractNeXMLDataReceiver {
 	private NeXMLTokenSetEventReceiver receiver;
 	private Set<Character> tokens = new HashSet<Character>();
-	
-	
+
+
 	public NeXMLMolecularDataTokenDefinitionReceiver(XMLStreamWriter writer, ReadWriteParameterMap parameterMap,
 			NeXMLWriterStreamDataProvider streamDataProvider) {
 		super(writer, parameterMap, streamDataProvider);
 		receiver = new NeXMLTokenSetEventReceiver(writer, parameterMap, streamDataProvider);
 	}
-	
-	
-	private void writeDNATokenDefinitions(NeXMLTokenSetEventReceiver receiver) throws IOException, XMLStreamException {	
+
+
+	private void writeDNATokenDefinitions(NeXMLTokenSetEventReceiver receiver) throws IOException, XMLStreamException {
 		Set<Character> remainingTokens = SequenceUtils.getNucleotideCharacters();
 		remainingTokens.removeAll(tokens);
 		remainingTokens.remove('U');
-		
+
 		List<String> states = new ArrayList<String>();
 		for (int i = 0; i < SequenceUtils.DNA_CHARS.length(); i++) {
 			states.add(Character.toString(SequenceUtils.DNA_CHARS.charAt(i)));
 		}
-		
+
 		writeTokenDefinitionEvents(receiver, remainingTokens, CharacterStateSetType.DNA, states);
 	}
-	
-	
+
+
 	private void writeRNATokenDefinitions(NeXMLTokenSetEventReceiver receiver) throws IOException, XMLStreamException {
 		Set<Character> remainingTokens = SequenceUtils.getNucleotideCharacters();
 		remainingTokens.removeAll(tokens);
 		remainingTokens.remove('T');
-		
+
 		List<String> states = new ArrayList<String>();
 		for (int i = 0; i < SequenceUtils.RNA_CHARS.length(); i++) {
 			states.add(Character.toString(SequenceUtils.RNA_CHARS.charAt(i)));
 		}
-		
+
 		writeTokenDefinitionEvents(receiver, remainingTokens, CharacterStateSetType.RNA, states);
 	}
-	
-	
+
+
 	private void writeAminoAcidTokenDefinitions(NeXMLTokenSetEventReceiver receiver) throws IOException, XMLStreamException {
 		Set<Character> remainingTokens = SequenceUtils.getAminoAcidOneLetterCodes(true);
 		remainingTokens.removeAll(tokens);
 		remainingTokens.remove('J');
-		
+
 		List<String> states = new ArrayList<String>();
 		for (Character state : SequenceUtils.getAminoAcidOneLetterCodes(false)) {
 			states.add(Character.toString(state));
 		}
-		
+
 		writeTokenDefinitionEvents(receiver, remainingTokens, CharacterStateSetType.AMINO_ACID, states);
 	}
-	
-	
-	private void writeTokenDefinitionEvents(NeXMLTokenSetEventReceiver receiver, Set<Character> remainingTokens, CharacterStateSetType alignmentType, 
+
+
+	private void writeTokenDefinitionEvents(NeXMLTokenSetEventReceiver receiver, Set<Character> remainingTokens, CharacterStateSetType alignmentType,
 			Collection<String> atomicStates) throws IOException, XMLStreamException {
 		List<String> constituents;
 		CharacterSymbolType type;
-		
+
 		for (Character token : remainingTokens) {
 			constituents = null;
 			type = CharacterSymbolType.ATOMIC_STATE;
 			char[] constituentChars = new char[0];
-			
+
 			if (alignmentType.equals(CharacterStateSetType.DNA)) {
 				constituentChars = SequenceUtils.nucleotideConstituents(token);
 			}
@@ -121,35 +121,35 @@ public class NeXMLMolecularDataTokenDefinitionReceiver extends AbstractNeXMLData
 			else if (alignmentType.equals(CharacterStateSetType.AMINO_ACID)) {
 				constituentChars = SequenceUtils.oneLetterAminoAcidConstituents(Character.toString(token));
 			}
-			
+
 			if (constituentChars.length > 1) {
 				constituents = new ArrayList<String>();
 				type = CharacterSymbolType.UNCERTAIN;
 				for (int i = 0; i < constituentChars.length; i++) {
 					constituents.add(Character.toString(constituentChars[i]));
-				}				
+				}
 			}
-			receiver.doAdd(new SingleTokenDefinitionEvent(getStreamDataProvider().createNewID(ReadWriteConstants.DEFAULT_TOKEN_DEFINITION_ID_PREFIX), 
+			receiver.doAdd(new SingleTokenDefinitionEvent(getStreamDataProvider().createNewID(ReadWriteConstants.DEFAULT_TOKEN_DEFINITION_ID_PREFIX),
 					null, Character.toString(token), CharacterSymbolMeaning.CHARACTER_STATE, type, constituents));
 			receiver.doAdd(ConcreteJPhyloIOEvent.createEndEvent(EventContentType.SINGLE_TOKEN_DEFINITION));
 		}
-		
+
 		receiver.doAdd(new SingleTokenDefinitionEvent(getStreamDataProvider().createNewID(ReadWriteConstants.DEFAULT_TOKEN_DEFINITION_ID_PREFIX),
 				"gap", Character.toString(SequenceUtils.GAP_CHAR), CharacterSymbolMeaning.GAP, CharacterSymbolType.UNCERTAIN, null));
 		receiver.doAdd(ConcreteJPhyloIOEvent.createEndEvent(EventContentType.SINGLE_TOKEN_DEFINITION));
-		
+
 		receiver.doAdd(new SingleTokenDefinitionEvent(getStreamDataProvider().createNewID(ReadWriteConstants.DEFAULT_TOKEN_DEFINITION_ID_PREFIX),
 				"missing data", Character.toString(SequenceUtils.MISSING_DATA_CHAR), CharacterSymbolMeaning.MISSING, CharacterSymbolType.UNCERTAIN, atomicStates));
 		receiver.doAdd(ConcreteJPhyloIOEvent.createEndEvent(EventContentType.SINGLE_TOKEN_DEFINITION));
-		
+
 		if (alignmentType.equals(CharacterStateSetType.AMINO_ACID)) {
 			receiver.doAdd(new SingleTokenDefinitionEvent(getStreamDataProvider().createNewID(ReadWriteConstants.DEFAULT_TOKEN_DEFINITION_ID_PREFIX),
 					"stop codon", Character.toString(SequenceUtils.STOP_CODON_CHAR), CharacterSymbolMeaning.OTHER, CharacterSymbolType.ATOMIC_STATE, null));
 			receiver.doAdd(ConcreteJPhyloIOEvent.createEndEvent(EventContentType.SINGLE_TOKEN_DEFINITION));
 		}
 	}
-	
-	
+
+
 	public void addRemainingEvents(CharacterStateSetType type) throws IOException, XMLStreamException {
 		switch (type) {
 			case NUCLEOTIDE:
@@ -164,9 +164,9 @@ public class NeXMLMolecularDataTokenDefinitionReceiver extends AbstractNeXMLData
 				break;
 			default:
 				break;
-		}		
+		}
 	}
-	
+
 
 	@Override
 	protected void handleLiteralMetaStart(LiteralMetadataEvent event) throws IOException, XMLStreamException {
@@ -204,14 +204,14 @@ public class NeXMLMolecularDataTokenDefinitionReceiver extends AbstractNeXMLData
 			case SINGLE_TOKEN_DEFINITION:
 				if (event.getType().getTopologyType().equals(EventTopologyType.START)) {
 					SingleTokenDefinitionEvent tokenDefinitionEvent = event.asSingleTokenDefinitionEvent();
-					
+
 					if (getStreamDataProvider().getAlignmentType().equals(CharacterStateSetType.AMINO_ACID)) {
-						if (tokenDefinitionEvent.getTokenName().length() > 1) { //only one letter codes can be written to NeXML
-							tokens.add(SequenceUtils.oneLetterAminoAcidByThreeLetter(tokenDefinitionEvent.getTokenName())); //Token must be a valid three letter code which was already checked in NeXMLCollectTokenSetDefinitionDataReceiver
+						if (tokenDefinitionEvent.getTokenName().length() > 1) {  //only one letter codes can be written to NeXML
+							tokens.add(SequenceUtils.oneLetterAminoAcidByThreeLetter(tokenDefinitionEvent.getTokenName()));  //Token must be a valid three letter code which was already checked in NeXMLCollectTokenSetDefinitionDataReceiver
 						}
 					}
 					else {
-						tokens.add(tokenDefinitionEvent.getTokenName().charAt(0));						
+						tokens.add(tokenDefinitionEvent.getTokenName().charAt(0));
 					}
 				}
 				receiver.doAdd(event);
@@ -219,5 +219,5 @@ public class NeXMLMolecularDataTokenDefinitionReceiver extends AbstractNeXMLData
 				break;
 		}
 		return true;
-	}	
+	}
 }
