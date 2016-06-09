3c0638efb570de732316e9dd6f4913e186a1ee49
SarahWiechers <wiechers@bioinfweb.info>
2016-06-09 12:00:48 +0000

    Added NeXML test case with multiple elements of various types.

--- test/src/info/bioinfweb/jphyloio/formats/nexml/NeXMLEventReaderTest.java
+++ test/src/info/bioinfweb/jphyloio/formats/nexml/NeXMLEventReaderTest.java
@@ -32,11 +32,7 @@ import info.bioinfweb.jphyloio.events.type.EventTopologyType;
 import info.bioinfweb.jphyloio.events.type.EventType;
 import info.bioinfweb.jphyloio.exception.JPhyloIOReaderException;
 
-import java.awt.Color;
 import java.io.File;
-import java.io.IOException;
-
-import javax.xml.stream.XMLStreamException;
 
 import org.junit.Test;
 
@@ -48,17 +44,14 @@ public class NeXMLEventReaderTest implements NeXMLConstants, ReadWriteConstants 
 		try {
 			ReadWriteParameterMap parameters = new ReadWriteParameterMap();
 			parameters.put(ReadWriteParameterMap.KEY_NEXML_TOKEN_TRANSLATION_STRATEGY, TokenTranslationStrategy.SYMBOL_TO_LABEL);
-			NeXMLEventReader reader = new NeXMLEventReader(new File("data/NeXML/SimpleDocument.xml"), parameters);
+			NeXMLEventReader reader = new NeXMLEventReader(new File("data/NeXML/MultipleElements.xml"), parameters);
 			try {
 				while (reader.hasNextEvent()) {
 					JPhyloIOEvent event = reader.next();
-//					System.out.println(event.getType());
+					System.out.println(event.getType());
 					
 					if (event.getType().equals(new EventType(EventContentType.META_RESOURCE, EventTopologyType.START))) {
 //					System.out.println("Predicate: " + event.asResourceMetadataEvent().getRel().getURI());
-					}
-					else if (event.getType().equals(new EventType(EventContentType.CHARACTER_SET_INTERVAL, EventTopologyType.SOLE))) {
-//						System.out.println("Start: " + event.asCharacterSetIntervalEvent().getStart() + " End: " + event.asCharacterSetIntervalEvent().getEnd());
 					}
 				}
 			}
@@ -373,248 +366,6 @@ public class NeXMLEventReaderTest implements NeXMLConstants, ReadWriteConstants 
 			reader.close();
 		}
 	}
-	
-	
-//	@Test
-//	public void testReadingMetaElements() {
-//		try {
-//			NeXMLEventReader reader = new NeXMLEventReader(new File("data/NeXML/MetaElements.xml"), false);
-//			try {
-//				assertEventType(EventContentType.DOCUMENT, EventTopologyType.START, reader);
-//				assertMetaEvent("id", "S794", true, true, reader);
-//				
-//				assertMetaEvent("tb:identifier.analysis", "http://purl.org/phylo/treebase/phylows/study/TB2:A1269", false, true, reader);
-//				assertMetaEvent("id", "meta1833", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertMetaEvent("tb:identifier.analysisstep", "http://purl.org/phylo/treebase/phylows/study/TB2:As1269", false, true, reader);
-//				assertMetaEvent("id", "meta1834", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertMetaEvent("tb:identifier.algorithm", "http://purl.org/phylo/treebase/phylows/study/TB2:Al1269", false, true, reader);
-//				assertMetaEvent("id", "meta1837", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertMetaEvent("dc:description", "neighbor joining", false, true, reader);
-//				assertMetaEvent("id", "meta1839", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertMetaEvent("dc:publisher", "Mycologia", false, true, reader);
-//				assertMetaEvent("id", "meta17", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.OTU_LIST, EventTopologyType.START, reader);
-//				assertMetaEvent("id", "Tls10691", true, true, reader);
-//				
-//				assertBasicOTUEvent(EventContentType.OTU, "Grifola sordulenta", "Tl126179", reader);
-//				
-//				assertMetaEvent("tb:identifier.taxonVariant", "29804", false, true, reader);
-//				assertMetaEvent("id", "meta23", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertMetaEvent("skos:closeMatch", "http://purl.uniprot.org/taxonomy/172312" , false, true, reader);
-//				assertMetaEvent("id", "meta22", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.OTU, EventTopologyType.END, reader);
-//				
-//				assertBasicOTUEvent(EventContentType.OTU, "Grifola frondosa WC836", "Tl261", reader);
-//				
-//				assertMetaEvent("tb:identifier.taxonVariant", "29801" , false, true, reader);
-//				assertMetaEvent("id", "meta30", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertMetaEvent("skos:closeMatch", "http://purl.uniprot.org/taxonomy/5627" , false, true, reader);
-//				assertMetaEvent("id", "meta29", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.OTU, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.OTU_LIST, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.ALIGNMENT, EventTopologyType.START, reader);
-//				assertMetaEvent("id", "M83", true, true, reader);
-//				
-//				assertEventType(EventContentType.TOKEN_SET_DEFINITION, EventTopologyType.SOLE, reader);
-//				
-//				assertMetaEvent("tb:type.matrix", "DNA" , false, true, reader);
-//				assertMetaEvent("id", "meta1615", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertBasicOTUEvent(EventContentType.SEQUENCE, "Grifola sordulenta", "Tl126179", reader);
-//				assertMetaEvent("id", "row1563", true, true, reader);
-//				assertCharactersEvent("AG?TCTGAAACGG--TGTAG", reader);
-//				assertEventType(EventContentType.SEQUENCE, EventTopologyType.END, reader);
-//				
-//				assertBasicOTUEvent(EventContentType.SEQUENCE, "Grifola frondosa WC836", "Tl261", reader);
-//				assertMetaEvent("id", "row1564", true, true, reader);
-//				assertCharactersEvent("AGTT--GAAAAGGGT?GTCG", reader);
-//				assertEventType(EventContentType.SEQUENCE, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.ALIGNMENT, EventTopologyType.END, reader);
-//				
-//				assertMetaEvent("rdfs:isDefinedBy", "http://purl.org/phylo/treebase/phylows/study/TB2:S794" , false, true, reader);
-//				assertMetaEvent("id", "meta1621", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.TREE, EventTopologyType.START, reader);
-//				assertMetaEvent("id", "Tr3926", true, true, reader);
-//				
-//				assertMetaEvent("tb:output.analysisstep", "http://purl.org/phylo/treebase/phylows/study/TB2:As1269" , false, true, reader);
-//				assertMetaEvent("id", "meta1841", true, true, reader);
-//				assertEventType(EventContentType.META_INFORMATION, EventTopologyType.END, reader);
-//				
-//
-//				assertLinkedOTUEvent(null, reader);
-//				assertEventType(EventContentType.NODE, EventTopologyType.END, reader);
-//
-//				assertLinkedOTUEvent("Grifola frondosa", reader);
-//				assertEventType(EventContentType.NODE, EventTopologyType.END, reader);
-//
-//				assertLinkedOTUEvent(null, reader);
-//				assertEventType(EventContentType.NODE, EventTopologyType.END, reader);
-//				
-//				assertEdgeEvent("Tn274900", "Tn274857", reader);
-//				assertMetaEvent("id", "edge1", true, true, reader);
-//				assertEventType(EventContentType.EDGE, EventTopologyType.END, reader);
-//				
-//				assertEdgeEvent("Tn274900", "Tn274833", reader);
-//				assertMetaEvent("id", "edge2", true, true, reader);
-//				assertEventType(EventContentType.EDGE, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.TREE, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.DOCUMENT, EventTopologyType.END, reader);
-//			}
-//			finally {
-//				reader.close();
-//			}
-//		}
-//		catch (Exception e) {
-//			e.printStackTrace();
-//			fail(e.getLocalizedMessage());
-//		}
-//	}
-	
-	
-//	@Test
-//	public void testReadingUnknownTags() {
-//		try {
-//			NeXMLEventReader reader = new NeXMLEventReader(new File("data/NeXML/UnknownTag.xml"), false);
-//			try {
-//				assertEventType(EventContentType.DOCUMENT, EventTopologyType.START, reader);
-//				assertMetaEvent("id", "test", true, true, reader);
-//				
-//				assertEventType(EventContentType.OTU_LIST, EventTopologyType.START, reader);
-//				assertMetaEvent("id", "taxa", true, true, reader);
-//				
-//				assertLinkedOTUEvent(EventContentType.OTU, null, "taxon1", "taxon1", reader);
-//				assertEventType(EventContentType.OTU, EventTopologyType.END, reader);
-//				
-//				assertBasicOTUEvent(EventContentType.OTU, null, "taxon2", reader);
-//				assertEventType(EventContentType.OTU, EventTopologyType.END, reader);
-//				
-//				assertBasicOTUEvent(EventContentType.OTU, null, "taxon3", reader);
-//				assertEventType(EventContentType.OTU, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.OTU_LIST, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.ALIGNMENT, EventTopologyType.START, reader);
-//				assertMetaEvent("id", "alignment", true, true, reader);
-//				
-//				assertEventType(EventContentType.TOKEN_SET_DEFINITION, EventTopologyType.SOLE, reader);
-//				
-//				assertBasicOTUEvent(EventContentType.SEQUENCE, "row1", "row1", reader);
-//				assertMetaEvent("id", "row1", true, true, reader);
-//				assertCharactersEvent("AG?TCTGAAACGG--TGTAG", reader);
-//				assertEventType(EventContentType.SEQUENCE, EventTopologyType.END, reader);
-//				
-//				assertBasicOTUEvent(EventContentType.SEQUENCE, "row2", "row2", reader);
-//				assertMetaEvent("id", "row2", true, true, reader);
-//				assertCharactersEvent("AGTT--GAAAAGGGT?GTCG", reader);
-//				assertEventType(EventContentType.SEQUENCE, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.ALIGNMENT, EventTopologyType.END, reader);
-//				
-//<<<<<<< .mine
-//				assertEventType(EventContentType.TREE, EventTopologyType.START, reader);
-//				assertMetaEvent("id", "tree1", true, true, reader);
-//				
-//				assertNodeEvent("taxon1", reader);
-//=======
-//				assertLinkedOTUEvent(null, reader);
-//>>>>>>> .r221
-//				assertEventType(EventContentType.NODE, EventTopologyType.END, reader);
-//<<<<<<< .mine
-//				
-//				assertNodeEvent("taxon2", reader);
-//=======
-//				assertLinkedOTUEvent(null, reader);
-//>>>>>>> .r221
-//				assertEventType(EventContentType.NODE, EventTopologyType.END, reader);
-//<<<<<<< .mine
-//				
-//				assertNodeEvent("internal1", reader);
-//=======
-//				assertLinkedOTUEvent(null, reader);
-//>>>>>>> .r221
-//				assertEventType(EventContentType.NODE, EventTopologyType.END, reader);
-//				
-//				assertEdgeEvent("internal1", "taxon1", 5.0, reader);
-//				assertMetaEvent("id", "edge1", true, true, reader);
-//				assertEventType(EventContentType.EDGE, EventTopologyType.END, reader);
-//				
-//				assertEdgeEvent("internal1", "taxon2", reader);
-//				assertMetaEvent("id", "edge2", true, true, reader);
-//				assertEventType(EventContentType.EDGE, EventTopologyType.END, reader);
-//				
-//<<<<<<< .mine
-//				assertEventType(EventContentType.TREE, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.TREE, EventTopologyType.START, reader);
-//				assertMetaEvent("id", "tree2", true, true, reader);
-//				
-//				assertNodeEvent("taxon1", reader);
-//=======
-//				assertLinkedOTUEvent(null, reader);
-//>>>>>>> .r221
-//				assertEventType(EventContentType.NODE, EventTopologyType.END, reader);
-//<<<<<<< .mine
-//				
-//				assertNodeEvent("taxon2", reader);
-//=======
-//				assertLinkedOTUEvent(null, reader);
-//>>>>>>> .r221
-//				assertEventType(EventContentType.NODE, EventTopologyType.END, reader);
-//<<<<<<< .mine
-//				
-//				assertNodeEvent("internal1", reader);
-//=======
-//				assertLinkedOTUEvent(null, reader);
-//>>>>>>> .r221
-//				assertEventType(EventContentType.NODE, EventTopologyType.END, reader);
-//				
-//				assertEdgeEvent("edge1", "internal1", reader);
-//				assertMetaEvent("id", "edge1", true, true, reader);
-//				assertEventType(EventContentType.EDGE, EventTopologyType.END, reader);
-//				
-//				assertEdgeEvent("edge2", "taxon1", 5.0, reader);
-//				assertMetaEvent("id", "edge2", true, true, reader);
-//				assertEventType(EventContentType.EDGE, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.TREE, EventTopologyType.END, reader);
-//				
-//				assertEventType(EventContentType.DOCUMENT, EventTopologyType.END, reader);
-//			}
-//			finally {
-//				reader.close();
-//			}
-//		}
-//		catch (Exception e) {
-//			e.printStackTrace();
-//			fail(e.getLocalizedMessage());
-//		}
-//	}
 	
 	
 	@Test
