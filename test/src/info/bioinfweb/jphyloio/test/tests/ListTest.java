d4bf01c631eba278be7a340e32e6663e2d318d4c
BenStoever <stoever@bioinfweb.info>
2016-06-28 10:22:38 +0000

    ListTest added.

---
+++ test/src/info/bioinfweb/jphyloio/test/tests/ListTest.java
@@ -0,0 +1,38 @@
+/*
+ * JPhyloIO - Event based parsing and stream writing of multiple sequence alignment and tree formats. 
+ * Copyright (C) 2015-2016  Ben Stöver, Sarah Wiechers
+ * <http://bioinfweb.info/JPhyloIO>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.jphyloio.test.tests;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+
+
+
+public class ListTest {
+	public static void main(String[] args) {
+		Collection<Object> list = new ArrayList<Object>();
+		//Collection<Object> list = new LinkedList<Object>();
+		list.add("Text");
+		list.add(18);
+		list.add(20.2);
+		list.add("problematic, text");
+		System.out.println(list);
+	}
+}
