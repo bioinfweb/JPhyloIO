b8b06a97d54a2140c51ab9f85dd1d571afd27abc
SarahWiechers <wiechers@bioinfweb.info>
2016-06-29 09:36:58 +0000

    Updated java doc.

--- main/src/info/bioinfweb/jphyloio/events/NodeEvent.java
+++ main/src/info/bioinfweb/jphyloio/events/NodeEvent.java
@@ -19,10 +19,21 @@
 package info.bioinfweb.jphyloio.events;
 
 
+import info.bioinfweb.jphyloio.JPhyloIOEventReader;
 import info.bioinfweb.jphyloio.events.type.EventContentType;
 
 
 
+/**
+ * Event indicating a node in a tree or network. If a node represents a root node of a tree, 
+ * {@link isRootNode()} will return {@code true}. A node in a network will never be a root node.
+ * <p>
+ * This event is a start event, which is followed by an end event of the same content type. Comment
+ * and metainformation events maybe nested between this and its according end event. (See the description
+ * of {@link JPhyloIOEventReader} for the complete grammar definition of JPhyloIO event streams.)
+ * 
+ * @author Sarah Wiechers
+ */
 public class NodeEvent extends LinkedLabeledIDEvent {
 	private boolean isRootNode;
 
@@ -32,8 +43,19 @@ public class NodeEvent extends LinkedLabeledIDEvent {
 		this.isRootNode = isRootNode;
 	}
 
-
+	
+	/**
+	 * Indicates whether this node is a true root of the tree it belongs to.
+	 * <p>
+	 * As opposed to the presence of a root edge, that merely specifies if an edge with a certain length 
+	 * leading to a root node should be displayed, a true root node is only present if the tree should be 
+	 * considered rooted and not only be displayed as rooted.
+	 * <p>
+	 * A network node will never be a true root.
+	 * 
+	 * @return {@code true} if this node is a true root or {@code false} otherwise
+	 */
 	public boolean isRootNode() {
 		return isRootNode;
-	}	
+	}
 }
