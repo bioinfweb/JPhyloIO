a6d7940a66aa51e2190baa97f16f49f38daedeb1
BenStoever <stoever@bioinfweb.info>
2016-09-22 09:13:03 +0000

    JavaDoc added to JPhyloIOFormatInfo.

--- main/src/info/bioinfweb/jphyloio/formats/JPhyloIOFormatInfo.java
+++ main/src/info/bioinfweb/jphyloio/formats/JPhyloIOFormatInfo.java
@@ -23,10 +23,37 @@ import info.bioinfweb.commons.io.ContentExtensionFileFilter;
 
 
 
+/**
+ * Classes implementing this interface provide information about a phylogenetic file format that is supported by <i>JPhyloIO</i>.
+ * 
+ * @author Ben St&ouml;ver
+ * @since 0.0.0
+ */
 public interface JPhyloIOFormatInfo {
+	/**
+	 * Returns the unique ID that identifies this format. IDs of formats supported by <i>JPhyloIO</i> are enumerated by 
+	 * {@link JPhyloIOFormatIDs}.
+	 * <p>
+	 * Third party developers that create readers or writers for additional formats must make sure to use a globally unique
+	 * format ID. It is strongly recommended to use owned reverse domain names for this (e.g. 
+	 * <code>org.example.additionalformat</code>).
+	 * 
+	 * @return the ID of the modeled format
+	 */
 	public String getFormatID();
 	
+	/**
+	 * Returns the human readable name of the modeled format. 
+	 * 
+	 * @return the name of the format
+	 */
 	public String getFormatName();
 	
+	/**
+	 * Creates a new file filter instance that is associated with the modeled format. It will check valid file extensions and 
+	 * possibly the contents of the file, if the extension is not unique.
+	 * 
+	 * @return a new file filter instance
+	 */
 	public ContentExtensionFileFilter createFileFilter();
 }
