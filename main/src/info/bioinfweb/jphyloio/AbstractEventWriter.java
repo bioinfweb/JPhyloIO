f659ca9c84b81bcc9bc135c108d3360dd02d7194
BenStoever <stoever@bioinfweb.info>
2016-11-15 10:00:36 +0000

    Minor changes.

--- main/src/info/bioinfweb/jphyloio/AbstractEventWriter.java
+++ main/src/info/bioinfweb/jphyloio/AbstractEventWriter.java
@@ -1,18 +1,18 @@
 /*
- * JPhyloIO - Event based parsing and stream writing of multiple sequence alignment and tree formats. 
+ * JPhyloIO - Event based parsing and stream writing of multiple sequence alignment and tree formats.
  * Copyright (C) 2015-2016  Ben Stöver, Sarah Wiechers
  * <http://bioinfweb.info/JPhyloIO>
- * 
+ *
  * This file is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -46,52 +46,52 @@ import java.util.Iterator;
 
 /**
  * Implements shared functionality for <i>JPhyloIO</i> event writers.
- * 
+ *
  * @author Ben St&ouml;ver
  */
 public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? extends AbstractEventWriter<P>>>	implements JPhyloIOEventWriter {
 	public static final String EDITED_LABEL_SEPARATOR = "_";
 
-	
+
 	public static interface UniqueLabelHandler {
 		public boolean isUnique(String label);
-		
+
 		public String editLabel(String label);
 	}
-	
-	
+
+
 	public static abstract class NoEditUniqueLabelHandler implements UniqueLabelHandler {
 		@Override
 		public String editLabel(String label) {
 			return label;
 		}
 	}
-	
-	
+
+
 	private String indention = "";
 	private P streamDataProvider;  // Must not be set to anything here.
-	
-	
+
+
 	public AbstractEventWriter() {
 		super();
 		streamDataProvider = createStreamDataProvider();
 	}
 
-	
+
 	/**
 	 * This method is called in the constructor of {@link AbstractEventWriter} to initialize the stream
 	 * data provider that will be returned by {@link #getStreamDataProvider()}. Inheriting classes that use
 	 * their own stream data provider implementation should overwrite this method.
 	 * <p>
 	 * This default implementation creates a new instance of {@link WriterStreamDataProvider}.
-	 * 
+	 *
 	 * @return the stream data provider to be used with this instance
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	protected P createStreamDataProvider() {
 		return (P)new WriterStreamDataProvider(this);  // Cannot be created generic, since this implementation is used by different inherited classes.
 	}
-	
+
 
 	public P getStreamDataProvider() {
 		return streamDataProvider;
@@ -100,20 +100,20 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 
 	/**
 	 * Writes the line separator, as it is specified in the parameter map or the line separator
-	 * of the current operating system, if the map contains no according entry. 
-	 * 
+	 * of the current operating system, if the map contains no according entry.
+	 *
 	 * @param writer the writer to write the line break to
 	 * @param parameters the parameter map possibly containing a line separator definition
 	 * @throws IOException if an I/O error occurs while writing to the specified writer
 	 */
 	public static void writeLineBreak(Writer writer, ReadWriteParameterMap parameters) throws IOException {
-		writer.write(parameters.getString(ReadWriteParameterMap.KEY_LINE_SEPARATOR, SystemUtils.LINE_SEPARATOR));
+		writer.write(parameters.getString(ReadWriteParameterNames.KEY_LINE_SEPARATOR, SystemUtils.LINE_SEPARATOR));
 	}
-	
-	
+
+
 	/**
 	 * Outputs a warning message, if the specified document data adapter provides one or more OTU lists.
-	 * 
+	 *
 	 * @param document the document data adapter to be checked for OTU lists
 	 * @param logger the logger to write the warning to
 	 * @param formatName the name of the format to which OTU lists cannot be written (will be included in the message)
@@ -121,20 +121,20 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 	 *        (The warning message will use this name for a hint that the OTU lists may still be used to label this
 	 *        type of elements.)
 	 */
-	public static void logIngnoredOTULists(DocumentDataAdapter document, ApplicationLogger logger, ReadWriteParameterMap parameters, 
+	public static void logIngnoredOTULists(DocumentDataAdapter document, ApplicationLogger logger, ReadWriteParameterMap parameters,
 			String formatName, String labeledElements) {
-		
+
 		if (document.getOTUListIterator(parameters).hasNext()) {
 			logger.addWarning("The specified OTU list(s) will not be written, since the " + formatName
 					+	" format does not support this. Referenced lists will though be used to try to label " + labeledElements
-					+ " if necessary."); 
+					+ " if necessary.");
 		}
 	}
-	
-	
+
+
 	/**
 	 * Returns a name for the specified event.
-	 * 
+	 *
 	 * @param event the event to get the name of
 	 * @return either the label of the event or its ID, if it does not carry a label
 	 */
@@ -146,8 +146,8 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 			return event.getID();
 		}
 	}
-	
-	
+
+
 	/**
 	 * Determines the name (label) to be used for the specified event when writing its data to a file. The name is
 	 * tried to be set by the following properties in decreasing priority:
@@ -156,15 +156,15 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 	 *   <li>the label of the linked OTU event, if any is linked and carries a label</li>
 	 *   <li>the ID of {@code linkedOTUEvent}</li>
 	 * </ol>
-	 * 
+	 *
 	 * @param linkedOTUEvent the event defining the name
 	 * @param otuList the data adapter providing the OTU data
 	 * @return a string representing the specified event as described
 	 * @see #getLinkedOTUNameOTUFirst(LinkedLabeledIDEvent, OTUListDataAdapter)
 	 */
-	public static String getLinkedOTUNameOwnFirst(LinkedLabeledIDEvent linkedOTUEvent, OTUListDataAdapter otuList, 
+	public static String getLinkedOTUNameOwnFirst(LinkedLabeledIDEvent linkedOTUEvent, OTUListDataAdapter otuList,
 			ReadWriteParameterMap parameters) {
-		
+
 		String result = linkedOTUEvent.getLabel();
 		if (result == null) {
 			if (linkedOTUEvent.hasLink() && (otuList != null)) {
@@ -177,8 +177,8 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 		return result;
 	}
 	//TODO Remove this method, if it us not used in the future.
-	
-	
+
+
 	/**
 	 * Determines the name (label) to be used for the specified event when writing its data to a file. The name is
 	 * tried to be set by the following properties in decreasing priority:
@@ -188,15 +188,15 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 	 *   <li>the label of {@code linkedOTUEvent} if present</li>
 	 *   <li>the ID of {@code linkedOTUEvent}</li>
 	 * </ol>
-	 * 
+	 *
 	 * @param linkedOTUEvent the event defining the name
 	 * @param otuList the data adapter providing the OTU data
 	 * @return a string representing the specified event as described
 	 * @see #getLinkedOTUNameOwnFirst(LinkedLabeledIDEvent, OTUListDataAdapter)
 	 */
-	public static String getLinkedOTUNameOTUFirst(LinkedLabeledIDEvent linkedOTUEvent, OTUListDataAdapter otuList, 
+	public static String getLinkedOTUNameOTUFirst(LinkedLabeledIDEvent linkedOTUEvent, OTUListDataAdapter otuList,
 			ReadWriteParameterMap parameters) {
-		
+
 		if (linkedOTUEvent.hasLink() && (otuList != null)) {
 			return getLabeledIDName(otuList.getObjectStartEvent(parameters, linkedOTUEvent.getLinkedID()));
 		}
@@ -204,19 +204,19 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 			return getLabeledIDName(linkedOTUEvent);
 		}
 	}
-	
-	
+
+
 	/**
 	 * Returns the OTU list found in {@code document} which is referenced by the specified event.
-	 * 
+	 *
 	 * @param document the document data adapter providing the OTU lists.
 	 * @param source the event referencing the OTU list
 	 * @return the referenced list or {@code null}, if the specified event does not reference any OTU
 	 * @throws IllegalArgumentException if no OTU list with the specified ID is available in {@code document}
 	 */
-	public static OTUListDataAdapter getReferencedOTUList(DocumentDataAdapter document, ElementDataAdapter<LinkedLabeledIDEvent> source, 
+	public static OTUListDataAdapter getReferencedOTUList(DocumentDataAdapter document, ElementDataAdapter<LinkedLabeledIDEvent> source,
 			ReadWriteParameterMap parameters) {
-		
+
 		OTUListDataAdapter result = null;
 		String otuListID = source.getStartEvent(parameters).getLinkedID();
 		if (otuListID != null) {
@@ -224,12 +224,12 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 		}
 		return result;
 	}
-	
-	
+
+
 	/**
 	 * Calculates the maximum sequence length in matrix with unequal lengths. This method iterates over all sequences
-	 * if {@link MatrixDataAdapter#getColumnCount(ReadWriteParameterMap)} returns -1, otherwise it will directly return the specified column count. 
-	 * 
+	 * if {@link MatrixDataAdapter#getColumnCount(ReadWriteParameterMap)} returns -1, otherwise it will directly return the specified column count.
+	 *
 	 * @param matrix the matrix data adapter containing the sequences
 	 * @return the maximal sequence length or -1, if the specified matrix data adapter does not declare any sequences
 	 */
@@ -243,38 +243,38 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 		}
 		return result;
 	}
-	
-	
+
+
 	protected String getIndention() {
 		return indention;
 	}
-	
-	
+
+
 	protected void writeLineStart(Writer writer, String text) throws IOException {
 		if (indention.length() > 0) {
 			writer.write(indention);
 		}
 		writer.write(text);
 	}
-	
-	
+
+
 	protected void increaseIndention() {
 		indention += "\t";
 	}
-	
-	
+
+
 	protected void decreaseIndention() {
 		if (indention.length() > 0) {
 			indention = indention.substring(1);
 		}
 	}
-	
-	
+
+
 	private static String createLabel(String prefix, String suffix, int maxLength, UniqueLabelHandler handler) {
 		prefix = handler.editLabel(prefix);
-		// Suffix is not edited, since it can only be "" or an integer value. (Problems would arise in formats which do not allow integers in names.) 
+		// Suffix is not edited, since it can only be "" or an integer value. (Problems would arise in formats which do not allow integers in names.)
 		if (suffix.length() > maxLength) {
-			throw new IllegalArgumentException("The label suffix \"" + suffix + 
+			throw new IllegalArgumentException("The label suffix \"" + suffix +
 					"\" is longer than the specified maximum length (" + maxLength + ").");  //TODO Throw some kind of writer exception instead?
 		}
 		else {
@@ -287,12 +287,12 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 			}
 		}
 	}
-	
-	
+
+
 	public static String createUniqueLabel(ReadWriteParameterMap parameters, UniqueLabelHandler handler, String label1,
 			String id1, String label2, String id2) {
-		
-		int maxLength = parameters.getInteger(ReadWriteParameterMap.KEY_MAXIMUM_NAME_LENGTH, Integer.MAX_VALUE);
+
+		int maxLength = parameters.getInteger(ReadWriteParameterNames.KEY_MAXIMUM_NAME_LENGTH, Integer.MAX_VALUE);
 		String result;
 		if (label1 != null) {
 			result = createLabel(label1, "", maxLength, handler);
@@ -304,7 +304,7 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 			result = id1;
 		}
 		String initialResult = result;
-		
+
 		if (!handler.isUnique(result)) {
 			if (label1 != null) {
 				if (label2 != null) {
@@ -314,13 +314,13 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 				if (!handler.isUnique(result)) {
 					result = id1 + EDITED_LABEL_SEPARATOR + label1;
 				}
-				
+
 				if (!handler.isUnique(result) && (label2 != null)) {
 					result = id1 + EDITED_LABEL_SEPARATOR + label1 + EDITED_LABEL_SEPARATOR + label2;
 				}
 			}
 		}
-		
+
 		if (!handler.isUnique(result)) {
 			result = initialResult;  // Do not append index on combined labels.
 			long suffix = 2;
@@ -331,50 +331,50 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 			}	while (!handler.isUnique(editedResult));
 			result = editedResult;
 		}
-		
+
 		return result;
 	}
-	
-	
+
+
 	public static String createUniqueLabel(ReadWriteParameterMap parameters, UniqueLabelHandler handler, LabeledIDEvent event) {
 		String result = createUniqueLabel(parameters, handler, event.getLabel(), event.getID(), null, null);
 		parameters.getLabelEditingReporter().addEdit(event, result);
 		return result;
 	}
 
-	
+
 	public static String createUniqueLabel(final ReadWriteParameterMap parameters, final LabeledIDEvent event) {
-		return createUniqueLabel(parameters, 
+		return createUniqueLabel(parameters,
 				new NoEditUniqueLabelHandler() {
 					@Override
 					public boolean isUnique(String label) {
 						return !parameters.getLabelEditingReporter().isLabelUsed(event.getType().getContentType(), label);
 					}
-				}, 
+				},
 				event);
 	}
-	
-	
-	public static String createUniqueLinkedOTULabel(ReadWriteParameterMap parameters, UniqueLabelHandler handler, 
+
+
+	public static String createUniqueLinkedOTULabel(ReadWriteParameterMap parameters, UniqueLabelHandler handler,
 			LinkedLabeledIDEvent event, OTUListDataAdapter otuList, boolean otuFirst) {
-		
+
 		if (event.hasLink() && (otuList != null)) {
 			try {
 				String result;
 				if (otuFirst) {
-					result = createUniqueLabel(parameters, handler, otuList.getObjectStartEvent(parameters, event.getLinkedID()).getLabel(), 
-							event.getLinkedID(), event.getLabel(), event.getID()); 
+					result = createUniqueLabel(parameters, handler, otuList.getObjectStartEvent(parameters, event.getLinkedID()).getLabel(),
+							event.getLinkedID(), event.getLabel(), event.getID());
 				}
 				else {
-					result = createUniqueLabel(parameters, handler, event.getLabel(), event.getID(), 
-							otuList.getObjectStartEvent(parameters, event.getLinkedID()).getLabel(), event.getLinkedID()); 
+					result = createUniqueLabel(parameters, handler, event.getLabel(), event.getID(),
+							otuList.getObjectStartEvent(parameters, event.getLinkedID()).getLabel(), event.getLinkedID());
 				}
 				parameters.getLabelEditingReporter().addEdit(event, result);
 				return result;
 			}
 			catch (IllegalArgumentException e) {  // from otuList.getOTUStartEvent()
-				throw new InconsistentAdapterDataException("The OTU with the ID " + event.getLinkedID() + 
-						" referenced by the data element with the ID " + event.getID() + 
+				throw new InconsistentAdapterDataException("The OTU with the ID " + event.getLinkedID() +
+						" referenced by the data element with the ID " + event.getID() +
 						" was not found in the OTU list with the ID " +	otuList.getStartEvent(parameters).getID());
 			}
 		}
@@ -383,7 +383,7 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 		}
 	}
 
-	
+
 //	protected String createUniqueLabel(ReadWriteParameterMap parameters, UniqueLabelTester tester, LabeledIDEvent event) {
 //		int maxLength = parameters.getInteger(ReadWriteParameterMap.KEY_MAXIMUM_NAME_LENGTH, Integer.MAX_VALUE);
 //		String result = createLabel(getLabeledIDName(event), "", maxLength);  //TODO Consider OTU
@@ -394,7 +394,7 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 //					result = alternative;
 //				}
 //			}
-//			
+//
 //			if (!tester.isUnique(result)) {
 //				long suffix = 2;
 //				String editedResult;
@@ -405,15 +405,15 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 //				result = editedResult;
 //			}
 //		}
-//		
+//
 //		parameters.getLabelEditingReporter().addEdit(event, result);
 //		return result;
 //	}
-	
-	
-	protected void extendSequence(MatrixDataAdapter matrix, ReadWriteParameterMap parameters, String sequenceID, long targetLength, 
+
+
+	protected void extendSequence(MatrixDataAdapter matrix, ReadWriteParameterMap parameters, String sequenceID, long targetLength,
 			String extensionToken, JPhyloIOEventReceiver receiver) throws IOException {
-		
+
 		if (extensionToken != null) {
 			long additionalLength = targetLength - matrix.getSequenceLength(parameters, sequenceID);
 			SingleSequenceTokenEvent startEvent = new SingleSequenceTokenEvent(null, extensionToken);
@@ -424,21 +424,21 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 			}
 		}
 	}
-	
-	
+
+
 	protected String getFileStartInfo(ReadWriteParameterMap parameters) {
 		StringBuffer applicationInfo = new StringBuffer();
 		applicationInfo.append("This file was generated by ");
-		
-		if (parameters.getString(ReadWriteParameterMap.KEY_APPLICATION_NAME) != null) {			
-			applicationInfo.append(parameters.getString(ReadWriteParameterMap.KEY_APPLICATION_NAME));			
+
+		if (parameters.getString(ReadWriteParameterMap.KEY_APPLICATION_NAME) != null) {
+			applicationInfo.append(parameters.getString(ReadWriteParameterMap.KEY_APPLICATION_NAME));
 			applicationInfo.append(" ");
-			
+
 			if (parameters.getObject(ReadWriteParameterMap.KEY_APPLICATION_VERSION, null) != null) {
 				applicationInfo.append(parameters.getObject(ReadWriteParameterMap.KEY_APPLICATION_VERSION, null).toString());
 				applicationInfo.append(" ");
 			}
-			
+
 			if (parameters.getString(ReadWriteParameterMap.KEY_APPLICATION_URL) != null) {
 				applicationInfo.append("<");
 				applicationInfo.append(parameters.getString(ReadWriteParameterMap.KEY_APPLICATION_URL));
@@ -451,38 +451,38 @@ public abstract class AbstractEventWriter<P extends WriterStreamDataProvider<? e
 
 		applicationInfo.append("using ");
 		applicationInfo.append(JPhyloIO.getInstance().getLibraryNameAndVersion());
-		applicationInfo.append(" <"); 
+		applicationInfo.append(" <");
 		applicationInfo.append(JPhyloIO.getInstance().getProjectURL().toString());
 		applicationInfo.append(">.");
-		
+
 		return applicationInfo.toString();
 	}
-	
-	
+
+
 	@Override
 	public void writeDocument(DocumentDataAdapter document, File file, ReadWriteParameterMap parameters) throws IOException {
 		Writer writer = new BufferedWriter(new FileWriter(file));
 		try {
-			writeDocument(document, writer, parameters);	
+			writeDocument(document, writer, parameters);
 		}
 		finally {
 			writer.close();
 		}
 	}
-	
+
 
 	@Override
-	public void writeDocument(DocumentDataAdapter document, OutputStream stream, ReadWriteParameterMap parameters) throws IOException {		
-		writeDocument(document, new BufferedWriter(new OutputStreamWriter(stream)), parameters);		
+	public void writeDocument(DocumentDataAdapter document, OutputStream stream, ReadWriteParameterMap parameters) throws IOException {
+		writeDocument(document, new BufferedWriter(new OutputStreamWriter(stream)), parameters);
 	}
 
 
 	@Override
 	public void writeDocument(DocumentDataAdapter document, Writer writer, ReadWriteParameterMap parameters) throws IOException {
-		parameters.put(ReadWriteParameterMap.KEY_WRITER_INSTANCE, this);
+		parameters.put(ReadWriteParameterNames.KEY_WRITER_INSTANCE, this);
 		doWriteDocument(document, writer, parameters);
 	}
-	
-	
+
+
 	protected abstract void doWriteDocument(DocumentDataAdapter document, Writer writer, ReadWriteParameterMap parameters) throws IOException;
 }
