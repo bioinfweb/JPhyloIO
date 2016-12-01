b187b13ce210285f054dc381457ed25dfcb98f0d
BenStoever <stoever@bioinfweb.info>
2016-12-01 09:09:44 +0000

    JPhyloIOEventFactory removed. JavaDoc extended.

--- main/src/info/bioinfweb/jphyloio/utils/package-info.java
+++ main/src/info/bioinfweb/jphyloio/utils/package-info.java
@@ -1,5 +1,6 @@
 /**
- * Contains general tool classes of <i>JPhyloIO</i>.
+ * Contains general tool classes of <i>JPhyloIO</i>. Some are of general use for application developers, while others are
+ * used internally as tool classes and can also be useful in developing third part event readers and writers. 
  * 
  * @author Ben St&ouml;ver
  */
