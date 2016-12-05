07f3708b2b3a6d47080469b5aa1c9364f8c0c596
BenStoever <stoever@bioinfweb.info>
2016-12-05 14:54:12 +0000

    Classes in main project restored.

---
+++ main/info.bioinfweb.jphyloio.core/src/info/bioinfweb/jphyloio/dataadapters/implementations/store/StoreTreeNetworkDataAdapter.java
@@ -0,0 +1,78 @@
+/*
+ * JPhyloIO - Event based parsing and stream writing of multiple sequence alignment and tree formats. 
+ * Copyright (C) 2015-2016  Ben Stöver, Sarah Wiechers
+ * <http://bioinfweb.info/JPhyloIO>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.jphyloio.dataadapters.implementations.store;
+
+
+import info.bioinfweb.jphyloio.ReadWriteParameterMap;
+import info.bioinfweb.jphyloio.dataadapters.ObjectListDataAdapter;
+import info.bioinfweb.jphyloio.dataadapters.TreeNetworkDataAdapter;
+import info.bioinfweb.jphyloio.events.EdgeEvent;
+import info.bioinfweb.jphyloio.events.JPhyloIOEvent;
+import info.bioinfweb.jphyloio.events.LabeledIDEvent;
+import info.bioinfweb.jphyloio.events.LinkedLabeledIDEvent;
+import info.bioinfweb.jphyloio.events.NodeEvent;
+
+import java.util.List;
+
+
+
+public class StoreTreeNetworkDataAdapter extends StoreAnnotatedDataAdapter<LinkedLabeledIDEvent> implements TreeNetworkDataAdapter {
+	private LabeledIDEvent startEvent;
+	private boolean isTree;
+	private StoreObjectListDataAdapter<NodeEvent> nodes = new StoreObjectListDataAdapter<NodeEvent>();
+	private StoreObjectListDataAdapter<EdgeEvent> edges = new StoreObjectListDataAdapter<EdgeEvent>();
+	private StoreObjectListDataAdapter<LinkedLabeledIDEvent> nodeEdgeSets = new StoreObjectListDataAdapter<LinkedLabeledIDEvent>();
+	
+	
+	public StoreTreeNetworkDataAdapter(LabeledIDEvent startEvent, boolean isTree, List<JPhyloIOEvent> annotations) {
+		super(annotations);
+		this.startEvent = startEvent;
+		this.isTree = isTree;
+	}
+
+
+	@Override
+	public LabeledIDEvent getStartEvent(ReadWriteParameterMap parameters) {
+		return startEvent;
+	}
+
+
+	@Override
+	public boolean isTree(ReadWriteParameterMap parameters) {
+		return isTree;
+	}
+
+
+	@Override
+	public StoreObjectListDataAdapter<NodeEvent> getNodes(ReadWriteParameterMap parameters) {
+		return nodes;
+	}
+
+
+	@Override
+	public StoreObjectListDataAdapter<EdgeEvent> getEdges(ReadWriteParameterMap parameters) {
+		return edges;
+	}
+
+
+	@Override
+	public ObjectListDataAdapter<LinkedLabeledIDEvent> getNodeEdgeSets(ReadWriteParameterMap parameters) {
+		return nodeEdgeSets;
+	}	
+}
---
+++ main/info.bioinfweb.jphyloio.core/src/info/bioinfweb/jphyloio/formats/nexus/commandreaders/trees/TreeReader.java
@@ -0,0 +1,77 @@
+/*
+ * JPhyloIO - Event based parsing and stream writing of multiple sequence alignment and tree formats. 
+ * Copyright (C) 2015-2016  Ben Stöver, Sarah Wiechers
+ * <http://bioinfweb.info/JPhyloIO>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.jphyloio.formats.nexus.commandreaders.trees;
+
+
+import java.io.EOFException;
+import java.io.IOException;
+
+import info.bioinfweb.commons.io.PeekReader;
+import info.bioinfweb.jphyloio.ReadWriteConstants;
+import info.bioinfweb.jphyloio.events.type.EventContentType;
+import info.bioinfweb.jphyloio.exception.JPhyloIOReaderException;
+import info.bioinfweb.jphyloio.formats.newick.NewickStringReader;
+import info.bioinfweb.jphyloio.formats.nexus.NexusConstants;
+import info.bioinfweb.jphyloio.formats.nexus.NexusReaderStreamDataProvider;
+import info.bioinfweb.jphyloio.formats.nexus.commandreaders.AbstractNexusCommandEventReader;
+
+
+
+public class TreeReader extends AbstractNexusCommandEventReader implements NexusConstants, ReadWriteConstants {
+	private NewickStringReader newickStringReader = null;
+	
+	
+	public TreeReader(NexusReaderStreamDataProvider nexusDocument) {
+		super(COMMAND_NAME_TREE, new String[]{BLOCK_NAME_TREES}, nexusDocument);
+	}
+
+	
+	@Override
+	protected boolean doReadNextEvent() throws IOException {
+		PeekReader reader = getStreamDataProvider().getDataReader();
+		
+		try {
+			if (newickStringReader == null) {  // First call
+				getStreamDataProvider().consumeWhiteSpaceAndComments();
+				String treeLabel = getStreamDataProvider().readNexusWord();
+				getStreamDataProvider().consumeWhiteSpaceAndComments();
+				
+				String treeGroupID = getStreamDataProvider().getSharedInformationMap().getString(
+						NexusReaderStreamDataProvider.INFO_KEY_CURRENT_BLOCK_ID);
+				getStreamDataProvider().getElementList(EventContentType.TREE, treeGroupID).add(treeLabel);
+				String treeID = DEFAULT_TREE_ID_PREFIX + getStreamDataProvider().getIDManager().createNewID();
+				getStreamDataProvider().getNexusNameToIDMap(EventContentType.TREE, treeGroupID).put(treeLabel, treeID);
+				
+				if (reader.peekChar() == KEY_VALUE_SEPARATOR) {
+					reader.read();  // Skip KEY_VALUE_SEPARATOR.
+					newickStringReader = new NewickStringReader(getStreamDataProvider(), treeID, treeLabel, 
+							new NexusNewickReaderNodeLabelProcessor(getStreamDataProvider()));
+				}
+				else {
+					throw new JPhyloIOReaderException("Expected \"" + KEY_VALUE_SEPARATOR + 
+							"\" behind the tree label in the TREE command, but found \"" + reader.peekChar() + "\".", reader);
+				}
+			}
+			return newickStringReader.addNextEvents();
+		}
+		catch (EOFException e) {
+			throw new JPhyloIOReaderException("Unexpected end of file inside a Nexus TREE command.", reader, e);
+		}
+	}
+}
