7445e553e9f52cc0c8fb2b33175cf10b446249b3
BenStoever <stoever@bioinfweb.info>
2018-02-26 13:46:41 +0000

    Version information updated.

--- main/info.bioinfweb.jphyloio.core/src/info/bioinfweb/jphyloio/JPhyloIO.java
+++ main/info.bioinfweb.jphyloio.core/src/info/bioinfweb/jphyloio/JPhyloIO.java
@@ -34,7 +34,7 @@ import java.net.URL;
  */
 public class JPhyloIO {
 	private static final String NAME = "JPhyloIO";
-	private static final ApplicationVersion VERSION = new ApplicationVersion(0, 5, 1, 1604, ApplicationType.ALPHA);
+	private static final ApplicationVersion VERSION = new ApplicationVersion(0, 5, 2, 1608, ApplicationType.ALPHA);  //TODO Set to beta with next increase of minor version number.
 
 	private static JPhyloIO firstInstance = null;
 	
